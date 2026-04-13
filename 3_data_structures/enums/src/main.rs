31144147d06d90d6d1a703405702faf0477b923e
Bakr Ouairem <neuralaether@Bakrs-MacBook-Pro.local>
2026-04-13 09:46:26 +0200

    Some enums and structs

---
+++ 3_data_structures/enums/src/main.rs
@@ -0,0 +1,33 @@
+enum IpAddrKind {
+        V4,
+        V6,
+    }
+use IpAddrKind::{V4, V6};
+
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let goog = IpAddr::V4(String::from("8.8.8.8"));
+    let mwrite = Message::Write(String::from("hello"));
+    let mmove = Message::Move { x: 1, y: 2 };
+    let mcolor = Message::ChangeColor(255, 0, 0);
+    let mquit = Message::Quit;
+}
